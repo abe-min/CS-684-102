dbcb73e3419bba7031179369d378ae4e5c569bfd
Abreham Mindaye <aam75@njit.edu>
2022-05-10 01:13:44 -0400

    Sprint 4 test plan
    Sprint 4 system testing test pla

--- HW3/CS684_HW3_Mind_Benders_display_text_Amulya.js
+++
@@ -1,9 +0,0 @@
-// Display the hash string reply from the API
-
-function display_string() {
-    //console.log(string_display);
-    $('h9').text(string_display);
-    $('h9').css('color', 'blue');
-  }
-          
-  
